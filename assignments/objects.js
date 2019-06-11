fc30008dbcc0f9c81d1f79e45a30af907d42818b
Duraan Ali <duraan522@gmail.com>
2019-06-10 19:25:26 -0500

    First Commit

--- assignments/objects.js
+++ assignments/objects.js
@@ -10,32 +10,71 @@
 // 5,adaine5@samsung.com,Antonietta,F
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
-const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
-}
+// const example = {
+//   "id": 0,
+//   "name": "Example",
+//   "email": "examples@you.edu",
+//   "gender": "F"
+// }
 
 // Write your intern objects here:
+const mitzi = { 
+  "id":1, 
+  "email":"mmelloy0@psu.edu", 
+  "first_name":"Mitzi", 
+  "gender":'F' 
+};
 
+const kennan = {
+    "id": 2, 
+    "email": "kdiben1@tinypic.com", 
+    "first_name": "Kennan", 
+    "gender": 'M', 
+     speak : function () {
+      return "Hello, my name is" + this.first_name + "";
+    }
+  };
+
+const keven = { 
+  "id":3,
+  "email":"kmummery2@wikimedia.org", 
+  "first_name":"Keven", 
+  "gender": 'M'
+};
+
+const gannie = {
+  "id":4, 
+  "email":"gmartinson3@illinois.edu", 
+  "first_name":"Gannie", 
+  "gender": 'M' 
+};
+
+const kennan = {
+  "id":5, 
+  "email":"adaine5@samsung.com", 
+  "first_name":"Antonietta", 
+  "gender": 'F'
+};
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(interns[0].first_name);
 // Kennan's ID
-
+console.log(interns[1].id);
 // Keven's email
-
+console.log(interns[2].email);
 // Gannie's name
-
+console.log(interns[3].first_name);
 // Antonietta's Gender
+console.log(interns[4].gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+
+console.log(interns[1].speak);
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
